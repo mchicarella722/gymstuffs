c8c26a843beb0e8d686711447dabae2348cb1f3a
Daniel Sloan <dsloan@client157-240.wireless.newpaltz.edu>
2017-12-19 15:36:22 -0500

    Login component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IndexComponent } from './index/index.component';
 import { ExerciseComponent } from './exercise/exercise.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,14 @@ import { ExerciseComponent } from './exercise/exercise.component';
     HeaderComponent,
     FooterComponent,
     IndexComponent,
-    ExerciseComponent
+    ExerciseComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: "home", component: IndexComponent },
-     // { path: "login", component: LoginrComponent },
+      { path: "login", component: LoginComponent },
      // { path: "you", component: YouComponent },
      // { path: "workouts", component: WorkoutsComponent },
      { path: "exercise", component: ExerciseComponent },

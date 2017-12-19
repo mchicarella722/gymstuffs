08f3cd0cd864e0ccfb045d0597a3eda626080e66
Daniel Sloan <dsloan@client157-240.wireless.newpaltz.edu>
2017-12-19 14:47:07 -0500

    Base Index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "home", component: IndexComponent },
+     // { path: "login", component: LoginrComponent },
+     // { path: "you", component: YouComponent },
+     // { path: "workouts", component: WorkoutsComponent },
+     // { path: "exercises", component: ExercisesComponent },
+     // { path: "sharing", component: SharingComponent }, 
+     // { path: "play", component: PlayComponent },
+      { path: "", pathMatch: "full", redirectTo: "/home" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

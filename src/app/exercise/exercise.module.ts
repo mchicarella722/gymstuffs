1c57b41e833b94f90d6c3041ebb2bd43cf98ac9f
Daniel Sloan <dsloan@client157-240.wireless.newpaltz.edu>
2017-12-19 14:47:45 -0500

    Exercise page

---
+++ src/app/exercise/exercise.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExerciseComponent } from './exercise.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExerciseComponent]
+})
+export class ExerciseModule { }
